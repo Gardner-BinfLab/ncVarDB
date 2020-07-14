060bc40d5af427194d94cfb9bcd0f7b8aaf2892a
Sacha <sacha@BIOCTeecessMBP2-2.staff.uod.otago.ac.nz>
2020-07-15 09:56:33 +1200

    size change

--- scripts/convert_to_forward.sh
+++ scripts/convert_to_forward.sh
@@ -1,5 +1,5 @@
 # this script converts "ref" and "alt" entries with "mutation type" "substitution" to bases that are on the forward strand. Insertions and deletions and one delin have been converted manually. 
-# WARNING this script will download a large file (hg38.2bit ~100GB) from UCSC database 
+# WARNING this script will download file hg38.2bit (835.4MB) from UCSC database 
 # run from a directory that contains: convert_to_forward.py
 # requires: python v3.7 and twoBitToFa (http://hgdownload.soe.ucsc.edu/admin/exe/)
 # To use twoBitToFa one needs to add specification to $HOME/.hg.conf file (http://genome.ucsc.edu/goldenPath/help/mysql.html)
