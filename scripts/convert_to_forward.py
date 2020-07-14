bbf9d2d48c0e20edc2dbf42b06d260df7cc3dc0d
Sacha <sacha@BIOCTeecessMBP2-2.staff.uod.otago.ac.nz>
2020-07-15 09:39:59 +1200

    convert pathogenic substitutions to forward

---
+++ scripts/convert_to_forward.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3.7
+import sys
+import pandas as pd
+from subprocess import check_output
+
+input_csv_file_name=sys.argv[1]
+output_csv_file_name=sys.argv[2]
+error_file_name=sys.argv[3]
+
+df=pd.read_csv(input_csv_file_name, sep=",", encoding = "ISO-8859-1")
+
+error_df=pd.DataFrame([], columns=df.columns)
+
+bases=["A", "G", "C", "T"]
+complements={"A":"T", "T":"A", "C":"G", "G":"C"}
+
+for index, row in df.iterrows():
+    if str(row["mut_type"])=="substitution":
+        reference=str(row["ref"]).upper()
+        mutant=str(row["alt"]).upper()
+        id=str(row["pmed_ID"])
+        chr=str(row["chr"])
+        if chr=="MT":
+            chr="M"
+        location=str(row["pos"]) ##todo adjust for multiples
+        position="chr"+chr+":"+str(int(location)-1)+"-"+location
+        try:
+            output=check_output(["twoBitToFa", "hg38.2bit:"+position, "stdout"])
+            forward_nuc=str(output).split('\\n')[1].upper()
+            if forward_nuc not in bases:
+                print("ERROR: unexpected base at " + location+ ": " + forward_nuc + ". Ignoring this entry.")
+            else:
+                if forward_nuc != reference:
+                    df.at[index,"ref"]=complements[reference]
+                    df.at[index,"alt"]=complements[mutant]
+        except:
+            error_df.append(row)
+            print("Could not fetch base at position: " + position)
+
+df.to_csv(output_csv_file_name, sep=",", mode='a', index=False)
+error_df.to_csv(error_file_name, index=False)
---
+++ scripts/convert_to_forward.sh
@@ -0,0 +1,12 @@
+# this script converts "ref" and "alt" entries with "mutation type" "substitution" to bases that are on the forward strand. Insertions and deletions and one delin have been converted manually. 
+# WARNING this script will download a large file (hg38.2bit ~100GB) from UCSC database 
+# run from a directory that contains: convert_to_forward.py
+# requires: python v3.7 and twoBitToFa (http://hgdownload.soe.ucsc.edu/admin/exe/)
+# To use twoBitToFa one needs to add specification to $HOME/.hg.conf file (http://genome.ucsc.edu/goldenPath/help/mysql.html)
+
+# adjust the path
+path_to_original_csv="../data/ncVar_pathogenic.csv"
+ 
+rsync -avzP rsync://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.2bit ./
+
+python3 convert_to_forward.py $path_to_original_csv ncVar_pathogenic_forward.csv convert_to_forward_error_path
