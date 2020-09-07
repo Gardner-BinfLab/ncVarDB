c213325c5233b7b29c8ca2344022268d6fe9f60d
Alexandra Gavryushkina <sasha@Alexandras-MBP.staff.uod.otago.ac.nz>
2020-09-08 08:53:04 +1200

    new benign

--- scripts/make_VCFs.R
+++ scripts/make_VCFs.R
@@ -10,49 +10,40 @@ wd = paste(main_directory_no_slash, "data", sep="/")
 setwd(wd)
 ##Make VCFs from current database version 
 
-Pathogenic <- read.csv("ncVar_pathogenic.csv")
+Pathogenic <- read.delim("ncVar_pathogenic.csv")
 
 path_VCF <- data.frame(Pathogenic$chr, Pathogenic$pos,Pathogenic$x_ref, Pathogenic$ref, Pathogenic$alt)
+
 path_VCF$QUAL <- 50
 path_VCF$PASS <- 'PASS'
 path_VCF$INFO <- Pathogenic$ID
 
-
-
-
 colnames(path_VCF) <- c("#CHR", "POS","ID", "REF", "ALT", "QUAL", "FILTER", "INFO")
-
-
 
 ##Remove the dbsnp string
 path_VCF$ID <- gsub("dbsnp:", "", path_VCF$ID)
 ##Add info heading
 path_VCF$INFO <- gsub('nc', 'NC=nc',path_VCF$INFO)
 
-
 ##Info header
 #'##INFO=<ID=NC,Number=.,Type=String,Description="ncVAR ID">'
 
 
-Benign <- read.csv("ncVar_benign.csv")
+Benign <- read.delim("ncVar_benign.csv")
 
 Ben_VCF <- data.frame(Benign$chr, Benign$pos,Benign$x_ref, Benign$ref, Benign$alt)
 Ben_VCF$QUAL <- 50
 Ben_VCF$PASS <- 'PASS'
 Ben_VCF$INFO <- Benign$ID
 
-
-
-
 colnames(Ben_VCF) <- c("#CHR", "POS","ID", "REF", "ALT", "QUAL", "FILTER", "INFO")
-
-
 
 ##Remove the dbsnp string
 Ben_VCF$ID <- gsub("dbsnp:", "", Ben_VCF$ID)
 ##Add info heading
 Ben_VCF$INFO <- gsub('nc', 'NC=nc',Ben_VCF$INFO)
 
+write.table(Ben_VCF, "benign_vcf.csv", quote = F,row.names = F,sep = "\t")
+write.table(path_VCF, "pathogenic_vcf.csv", quote = F,row.names = F,sep = "\t")
 
-write.csv(Ben_VCF,"benign_vcf.csv", quote = F, row.names = F)
-write.csv(path_VCF,"pathogenic_vcf.csv", quote = F, row.names = F)
+
--- scripts/make_vcf.sh
+++ scripts/make_vcf.sh
@@ -1,15 +1,9 @@
 ##Automatically update vcf files from most recent version of ncVar database
 #adjust path
-main_path="/Users/Sasha/Temp/Git/ncVarDB/"
+main_path="/Users/sasha/Git/ncVarDB/"
 
 Rscript make_VCFs.R $main_path
-cat $main_path'data/pathogenic_vcf.csv' | tr ',' '\t' >path_vcf_intermediate
-cat $main_path'data/benign_vcf.csv' | tr ',' '\t' >ben_vcf_intermediate
-cat $main_path'docs/vcf_meta_info.txt' path_vcf_intermediate >$main_path'data/ncVar_pathogenic.vcf'
-cat $main_path'docs/vcf_meta_info.txt' ben_vcf_intermediate >$main_path'data/ncVar_benign.vcf'
+cat $main_path'docs/vcf_meta_info.txt' $main_path'data/pathogenic_vcf.csv' >$main_path'data/ncVar_pathogenic.vcf'
+cat $main_path'docs/vcf_meta_info.txt' $main_path'data/benign_vcf.csv' >$main_path'data/ncVar_benign.vcf'
 rm $main_path'data/pathogenic_vcf.csv'
 rm $main_path'data/benign_vcf.csv'
-rm path_vcf_intermediate
-rm ben_vcf_intermediate
-
-
