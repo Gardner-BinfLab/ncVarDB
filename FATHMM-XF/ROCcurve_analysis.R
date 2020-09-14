535be809f23ec5248b2bc1211f0488820c75a2cc
Alexandra Gavryushkina <sasha@Alexandras-MBP.staff.uod.otago.ac.nz>
2020-09-15 10:08:31 +1200

    update FATHMM-XF

--- FATHMM-XF/ROCcurve_analysis.R
+++ FATHMM-XF/ROCcurve_analysis.R
@@ -15,9 +15,12 @@ benign <- read.csv(benign_filename, sep="\t", stringsAsFactors=FALSE)
 benign$ncVar_type <- "benign"
 all_variants <- rbind(pathogenic, benign)
 all_variants$Non.Coding.Score <- as.numeric(all_variants$Non.Coding.Score)
+not_scored_variants <- all_variants[is.na(all_variants$Non.Coding.Score),]
 all_variants <- all_variants[!is.na(all_variants$Non.Coding.Score),]
 
 
 pdf("FATHMM-XF_ROCcurve.pdf")
 plot.roc(all_variants$ncVar_type, all_variants$Non.Coding.Score, print.thres=TRUE, print.thres.col="red", print.auc=TRUE)
 dev.off()
+
+write.table(not_scored_variants, "FATHMM-XF_error.tsv", sep="\t", quote=F, row.names=F)
